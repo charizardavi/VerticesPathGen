470e1eebd90800f73ea298fcd2cda9fd64302c76
Riya Tyagi <59206566+Centrattic@users.noreply.github.com>
2023-08-23 09:53:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,147 +1,109 @@
-class EPointF {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
+// JavaScript equivalent of EPointF class (same as before)
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const backgroundImage = document.getElementById("background-image");
+
+const knots = [
+    new EPointF(100, 300),
+    new EPointF(300, 100),
+    new EPointF(500, 500),
+    new EPointF(700, 300)
+];
+
+const controlPoints = computeControlPoints(knots);
+
+function drawBezierCurve() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+    ctx.moveTo(knots[0].x, knots[0].y);
+
+    for (let i = 1; i < knots.length; i += 3) {
+        ctx.bezierCurveTo(
+            controlPoints[i - 1].x, controlPoints[i - 1].y,
+            controlPoints[i].x, controlPoints[i].y,
+            knots[i].x, knots[i].y
+        );
     }
 
-    plus(factor, ePointF) {
-        return new EPointF(this.x + factor * ePointF.x, this.y + factor * ePointF.y);
-    }
-
-    minus(factor, ePointF) {
-        return new EPointF(this.x - factor * ePointF.x, this.y - factor * ePointF.y);
-    }
-
-    scaleBy(factor) {
-        return new EPointF(factor * this.x, factor * this.y);
-    }
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 2;
+    ctx.stroke();
 }
 
-// Compute a smooth path with n cubic Bezier curves
-function computePathThroughKnots(knots) {
-    if (knots.length < 2) {
-        throw new Error("Collection must contain at least two knots");
-    }
-
-    const polyBezierPath = [];
-    const firstKnot = knots[0];
-    polyBezierPath.push(`M ${firstKnot.x} ${firstKnot.y}`);
-
+function computeControlPoints(knots) {
     const n = knots.length - 1;
+    const controlPoints = [];
 
-    if (n === 1) {
-        const lastKnot = knots[1];
-        polyBezierPath.push(`L ${lastKnot.x} ${lastKnot.y}`);
-    } else {
-        const controlPoints = computeControlPoints(n, knots);
+    for (let i = 0; i < n; i++) {
+        if (i === 0) {
+            controlPoints.push(knots[0]);
+            controlPoints.push(knots[0].plus(2 / 3, knots[1]));
+        } else {
+            const prevControl = controlPoints[controlPoints.length - 1];
+            const nextKnot = knots[i + 1];
 
-        for (let i = 0; i < n; i++) {
-            const targetKnot = knots[i + 1];
-            appendCurveToPath(polyBezierPath, controlPoints[i], controlPoints[n + i], targetKnot);
+            controlPoints.push(prevControl.plus(1 / 3, nextKnot.minus(prevControl)));
+            controlPoints.push(nextKnot);
         }
     }
 
-    return polyBezierPath.join(' ');
+    return controlPoints;
 }
 
-function computeControlPoints(n, knots) {
-    const result = new Array(2 * n);
-
-    const target = constructTargetVector(n, knots);
-    const lowerDiag = constructLowerDiagonalVector(n - 1);
-    const mainDiag = constructMainDiagonalVector(n);
-    const upperDiag = constructUpperDiagonalVector(n - 1);
-
-    const newTarget = new Array(n);
-    const newUpperDiag = new Array(n - 1);
-
-    newUpperDiag[0] = upperDiag[0] / mainDiag[0];
-    newTarget[0] = target[0].scaleBy(1 / mainDiag[0]);
-
-    for (let i = 1; i < n - 1; i++) {
-        newUpperDiag[i] = upperDiag[i] / (mainDiag[i] - lowerDiag[i - 1] * newUpperDiag[i - 1]);
-    }
-
-    for (let i = 1; i < n; i++) {
-        const targetScale = 1 / (mainDiag[i] - lowerDiag[i - 1] * newUpperDiag[i - 1]);
-        newTarget[i] = target[i].minus(newTarget[i - 1].scaleBy(lowerDiag[i - 1])).scaleBy(targetScale);
-    }
-
-    result[n - 1] = newTarget[n - 1];
-
-    for (let i = n - 2; i >= 0; i--) {
-        result[i] = newTarget[i].minus(newUpperDiag[i], result[i + 1]);
-    }
-
-    for (let i = 0; i < n - 1; i++) {
-        result[n + i] = knots[i + 1].scaleBy(2).minus(result[i + 1]);
-    }
-
-    result[2 * n - 1] = knots[n].plus(result[n - 1]).scaleBy(0.5);
-
-    return result;
+function updateKnotPosition(index, x, y) {
+    knots[index] = new EPointF(x, y);
+    controlPoints = computeControlPoints(knots);
+    drawBezierCurve();
 }
 
-function constructTargetVector(n, knots) {
-    const result = new Array(n);
+function drawKnots() {
+    ctx.fillStyle = "red";
 
-    result[0] = knots[0].plus(2, knots[1]);
-
-    for (let i = 1; i < n - 1; i++) {
-        result[i] = knots[i].scaleBy(2).plus(knots[i + 1]).scaleBy(2);
+    for (let i = 0; i < knots.length; i++) {
+        ctx.beginPath();
+        ctx.arc(knots[i].x, knots[i].y, 5, 0, Math.PI * 2);
+        ctx.fill();
     }
-
-    result[n - 1] = knots[n - 1].scaleBy(8).plus(knots[n]);
-
-    return result;
 }
 
-function constructLowerDiagonalVector(length) {
-    const result = new Array(length);
-
-    for (let i = 0; i < result.length - 1; i++) {
-        result[i] = 1;
+function findKnotIndex(x, y) {
+    for (let i = 0; i < knots.length; i++) {
+        if (Math.sqrt((knots[i].x - x) ** 2 + (knots[i].y - y) ** 2) <= 5) {
+            return i;
+        }
     }
-
-    result[result.length - 1] = 2;
-
-    return result;
+    return -1;
 }
 
-function constructMainDiagonalVector(n) {
-    const result = new Array(n);
+let selectedKnotIndex = -1;
+let isDragging = false;
 
-    result[0] = 2;
+canvas.addEventListener("mousedown", (e) => {
+    const x = e.clientX - canvas.getBoundingClientRect().left;
+    const y = e.clientY - canvas.getBoundingClientRect().top;
+    selectedKnotIndex = findKnotIndex(x, y);
 
-    for (let i = 1; i < result.length - 1; i++) {
-        result[i] = 4;
+    if (selectedKnotIndex !== -1) {
+        isDragging = true;
     }
+});
 
-    result[result.length - 1] = 7;
-
-    return result;
-}
-
-function constructUpperDiagonalVector(length) {
-    const result = new Array(length);
-
-    for (let i = 0; i < result.length; i++) {
-        result[i] = 1;
+canvas.addEventListener("mousemove", (e) => {
+    if (isDragging) {
+        const x = e.clientX - canvas.getBoundingClientRect().left;
+        const y = e.clientY - canvas.getBoundingClientRect().top;
+        updateKnotPosition(selectedKnotIndex, x, y);
     }
+});
 
-    return result;
-}
+canvas.addEventListener("mouseup", () => {
+    isDragging = false;
+    selectedKnotIndex = -1;
+});
 
-function appendCurveToPath(pathArray, control1, control2, targetKnot) {
-    pathArray.push(`C ${control1.x} ${control1.y}, ${control2.x} ${control2.y}, ${targetKnot.x} ${targetKnot.y}`);
-}
-
-// JavaScript equivalent of EPointF class (same as before)
-
-const knots = [];
-
-// JavaScript equivalent of EPointF class (same as before)
-
-const knots = [];
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+drawBezierCurve();
+drawKnots();
