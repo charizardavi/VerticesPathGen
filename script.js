09d88542228b9735d09f5472f1120fdf9e13e6e4
Riya Tyagi <59206566+Centrattic@users.noreply.github.com>
2023-08-23 12:30:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -154,13 +154,13 @@ function calculateDerivative(curve, deltaT, pointType) {
     equation = generateLineEquation(curve);
   }
   console.log(equation);
-  /*
+  
   const x1 = eval(equation.x.replace(/t/g, init.toString()));
   const y1 = eval(equation.y.replace(/t/g, init.toString()));
 
   const x2 = eval(equation.x.replace(/t/g, (init+change).toString()));
   const y2 = eval(equation.y.replace(/t/g, (init+change).toString()));
-  */
+  
   /*var t = init;
   const x1 = eval(equation.x);
   const y1 = eval(equation.y);
@@ -169,14 +169,6 @@ function calculateDerivative(curve, deltaT, pointType) {
   const x2 = eval(equation.x);
   const y2 = eval(equation.y);
   */
-
-  const equationFunction = new Function('t', `return {x: (${equation.x}), y: (${equation.y})};`);
-
-  const t = init;
-  const { x: x1, y: y1 } = equationFunction(t);
-
-  const t2 = init + change;
-  const { x: x2, y: y2 } = equationFunction(t2);
 
   const deltaX = (x2 - x1) / change;
   const deltaY = (y2 - y1) / change;
