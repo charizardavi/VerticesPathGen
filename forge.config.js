dd41399b32c3eff1544ea2cedc213584220d83d8
Avaninder Bhaghayath <charizardavi@gmail.com>
2023-08-24 14:54:48 -0400

    fixed electron

--- forge.config.js
+++ forge.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: "logo.ico"
   },
   rebuildConfig: {},
   makers: [
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { app, BrowserWindow } = require('electron');
-
+if (require('electron-squirrel-startup')) app.quit();
 function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
